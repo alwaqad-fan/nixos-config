b130cdb96bdd0664eb5424deb879ed8b81213e9e
senwix <kunykunykuny6@gmail.com>
2026-01-08 11:03:34 +0200

    Add files via upload

--- WMs/plasma.nix
+++ WMs/plasma.nix
@@ -2,7 +2,7 @@
 
 {
 
-   environment.desktopManager.plasma6.enable = true;
+   services.desktopManager.plasma6.enable = true;
    environment.plasma6.excludePackages = with pkgs.kdePackages;
     [
       kate
